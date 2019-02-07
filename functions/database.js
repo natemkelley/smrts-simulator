09960f8d1da8548ccf843ef4b0fee46db28827a8
Nate Kelley <31867265+natemkelley@users.noreply.github.com>
2019-02-07 11:27:12 -0700

    Sending an actual twitter via API

--- functions/database.js
+++ functions/database.js
@@ -1,3 +1,4 @@
+var functions = require('../functions/functions')
 var mongoose = require('mongoose');
 
 //define where to connect the database
@@ -21,7 +22,7 @@ mongoose.connection.on('error', function (err) {
 
 /***********TWITTER SIMULATIONS*************/
 //define the model and schema for twitter
-var functions = require('../models/twitterModel');
+var twitterModel = require('../models/twitterModel');
 var twitterSimulation = mongoose.model('twitterSimulation');
 
 function saveTwitterSimulation() {
@@ -33,22 +34,23 @@ function saveTwitterSimulation() {
     });
     testTwitterSimulation.save(function (err) {
         if (err) return handleError(err);
-        console.log('\nsaved tweet');
+        console.log('testing saved tweet successful'.green);
         getAllTwitterSimulation();
     });
 }
+
 function getAllTwitterSimulation() {
     var query = twitterSimulation.find({});
     query.then(function (doc) {
-        console.log(doc);
         removeAll();
     });
 }
+
 function removeAll() {
     var removeAll = twitterSimulation.deleteMany({});
     removeAll.then(function (log, err) {
         if (log) {
-            console.log(log)
+            console.log('deleting all tweets status '.yellow + JSON.stringify(log).yellow)
         }
     });
 }
